8a564fce9ad52582ead4077575e73d8f7fa9ae50
TrulsLundstrom <lutr22ix@student.ju.se>
2024-11-01 11:11:47 +0100

    added methods to CustomDialog
    jag la till två metoder. Just nu känns det som ganska få metoder, så i framtiden vill jag nog lägga till flera metoder så att denna klassen blir viktigare och får mer ansvar. Men jag upptäcker säkert fler användningsområden automatiskt när jag har skrivit mer kod.

--- app/src/main/java/com/example/finalprojectmultiplayertictactoe/CustomDialog.kt
+++ app/src/main/java/com/example/finalprojectmultiplayertictactoe/CustomDialog.kt
@@ -3,4 +3,14 @@ package com.example.finalprojectmultiplayertictactoe
 // visar meddelanden, som t.ex acceptera (eller tacka nej) till utmaningar.
 class CustomDialog{
 
+    // metod för att visa dialogen med ett meddelande och alternativ (accept eller decline)
+    fun showChallengeDiaglog(){ // FUNDERA VAD FÖR INPARAMETRAR SOM DU SEN KOMMER BEHÖVA HÄR
+
+    }
+
+    // ger information om t.ex spelets regler och hur appen fungerar, etc...
+    fun showInfoDialog(message: String){
+
+    }
+
 }
