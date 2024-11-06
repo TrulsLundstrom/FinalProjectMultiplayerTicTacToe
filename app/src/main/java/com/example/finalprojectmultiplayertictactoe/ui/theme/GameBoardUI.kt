ae63e2fef0849d78d8be3d73f331c02262753659
TrulsLundstrom <lutr22ix@student.ju.se>
2024-11-06 12:39:00 +0100

    board is more in center
    jag centrerade brädan och gjorde den större, jag gjorde även symbolerna större.

--- app/src/main/java/com/example/finalprojectmultiplayertictactoe/ui/theme/GameBoardUI.kt
+++ app/src/main/java/com/example/finalprojectmultiplayertictactoe/ui/theme/GameBoardUI.kt
@@ -2,10 +2,12 @@ package com.example.finalprojectmultiplayertictactoe.ui.theme
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 
 import androidx.compose.material3.Text
@@ -20,18 +22,25 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun GameBoard(boardState: Array<Array<String?>>, onCellClick: (Int, Int) -> Unit){
-    Column{
-        for(i in 0..2){
-            Row{
-                for(j in 0..2){
-                    Box(
-                        modifier = Modifier
-                            .size(64.dp)
-                            .border(1.dp, Color.Black)
-                            .clickable { onCellClick(i, j) },
-                        contentAlignment = Alignment.Center
-                    ){
-                        Text(boardState[i][j] ?: "", fontSize = 24.sp)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Column{
+            for(i in 0..2){
+                Row{
+                    for(j in 0..2){
+                        Box(
+                            modifier = Modifier
+                                .size(128.dp) // var innan 64
+                                .border(1.dp, Color.Black)
+                                .clickable { onCellClick(i, j) },
+                            contentAlignment = Alignment.Center
+                        ){
+                            Text(boardState[i][j] ?: "", fontSize = 48.sp) // var innan 24
+                        }
                     }
                 }
             }
