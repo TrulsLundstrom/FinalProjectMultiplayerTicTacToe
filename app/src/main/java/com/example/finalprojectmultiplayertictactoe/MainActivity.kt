a818a846af320dcadebf605275424f51f4ac0834
TrulsLundstrom <lutr22ix@student.ju.se>
2024-11-06 19:32:15 +0100

    new screen added and removed classes
    jag tog bort kommentarer som jag tyckte var onödiga. Jag tog bort 2st klass-filer som inte behövdes. En av dessa klasseras namn använde jag till en fil i ui.theme som heter "ResultScreen". Jag har även gjort en lobby som just nu inte har något innehåll.

--- app/src/main/java/com/example/finalprojectmultiplayertictactoe/MainActivity.kt
+++ app/src/main/java/com/example/finalprojectmultiplayertictactoe/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 
 import androidx.compose.runtime.getValue
 import androidx.compose.material3.Text
@@ -23,71 +24,84 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
 
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.finalprojectmultiplayertictactoe.ui.theme.ResultScreen
 
 
 // hanterar appens livscycel och navigerar
 class MainActivity : ComponentActivity(){
-
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
         setContent{
+            val navController = rememberNavController()
+
             var player1Name by remember { mutableStateOf<String?>(null) }
-            val player2Name by remember { mutableStateOf("Player 2") } // placeholder namn för spelare 2
-            var currentPlayer by remember { mutableStateOf("X") } // första symbolen som placeras ut är "X"
+            val player2Name = "Player 2" // placeholder-namn för spelare 2
+            var currentPlayer by remember { mutableStateOf("X") }
 
             var boardState by remember { mutableStateOf(Array(3) { arrayOfNulls<String>(3) }) }
-            val gameLogic = GameLogic() // objekt/instans av den nya klassen GameLogic som hanterar om det är vinnst/förlust/oavgjort
+            val gameLogic = GameLogic()
             var resultMessage by remember { mutableStateOf<String?>(null) }
 
-            if(player1Name == null){
-                PlayerNameInputScreen { enteredName ->
-                    player1Name = enteredName // sparar/lagrar spelarens namn
-                }
-            }
-            else{
+            NavHost(navController = navController, startDestination = "nameInput"){
+                composable("nameInput"){
 
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Text(
-                        text = if(currentPlayer == "X") "$player1Name's turn" else "$player2Name's turn",
-                        fontSize = 42.sp,
-                        modifier = Modifier.padding(top = 128.dp)
-                    )
-
-                    GameBoard(
-                        boardState = boardState,
-                        onCellClick = { x, y ->
-
-                            if(boardState[x][y] == null && resultMessage == null){ // om rutan är tom, så ska symbolen som representerar den nuvarande spelaren placeras i en ruta
-                                boardState = boardState.copyOf().apply { // skapar en ny kopia av boardState med uppdaterad ruta
-                                        this[x][y] = currentPlayer
-                                }
-
-                                if(gameLogic.checkWinner(boardState, currentPlayer)){
-                                    resultMessage = if(currentPlayer == "X"){
-                                        "$player1Name has won!" // spelare 1 har vunnit
-                                    }
-                                    else{
-                                        "$player2Name has won!" // spelare 2 har vunnit
-                                    }
-                                }
-                                else if(boardState.all { row -> row.all { it != null } }){ // om alla rutor är fyllda så blir det oavgjort
-                                    resultMessage = "It's a draw!"
-                                }
-                                else{
-                                    currentPlayer = if(currentPlayer == "X") "O" else "X" // växlar mellan "X" (spelare 1) och "O" (spelare 2)
-                                }
-                            }
+                    if(player1Name == null){
+                        PlayerNameInputScreen { enteredName ->
+                            player1Name = enteredName
                         }
-                    )
+                    }
+                    else{
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ){
+                            Text(
+                                text = if(currentPlayer == "X") "$player1Name's turn" else "$player2Name's turn",
+                                fontSize = 42.sp,
+                                modifier = Modifier.padding(top = 128.dp)
+                            )
+
+                            GameBoard(
+                                boardState = boardState,
+                                onCellClick = { x, y ->
+                                    if(boardState[x][y] == null && resultMessage == null){
+                                        boardState = boardState.copyOf().apply { this[x][y] = currentPlayer }
+
+                                        if(gameLogic.checkWinner(boardState, currentPlayer)){
+                                            resultMessage = if(currentPlayer == "X") "$player1Name has won!" else "$player2Name has won!"
+                                            navController.navigate("result")
+                                        }
+                                        else if(boardState.all { row -> row.all { it != null } }){
+                                            resultMessage = "It's a draw!"
+                                            navController.navigate("result")
+                                        }
+                                        else{
+                                            currentPlayer = if(currentPlayer == "X") "O" else "X"
+                                        }
+                                    }
+                                }
+                            )
+                        }
+                    }
                 }
-            }
-            resultMessage?.let {
-                Text(text = it)
+                composable("result"){
+                   ResultScreen(resultMessage = resultMessage, navController = navController)
+                }
+
+                composable("lobby"){
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ){
+                        Text(text = "Lobby (NOTHING HERE YET)", fontSize = 24.sp)
+                    }
+                }
             }
         }
     }
